8d5d929d85e8ed37c0f311349ddde0d6adc8cd23
Nicolás Antonelli <niconelli2@gmail.com>
2022-03-10 01:34:34 -0300

    Firs component (Layout)

---
+++ components/Layout.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+const Layout = (props: any) => (
+    <div id="layout">
+        <div className="test">TestLayout</div>
+        {props.children}
+    </div>
+)
+
+export default Layout
