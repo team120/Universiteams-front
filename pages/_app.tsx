af73ef1f595af7e63c62a02454156171e9398151
Nicolás Antonelli <niconelli2@gmail.com>
2022-03-24 01:32:11 -0300

    Theme: add preference in local storage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
 import type { AppProps } from 'next/app'
 
-import { useColorScheme, useHotkeys } from '@mantine/hooks'
+import { useColorScheme, useHotkeys, useLocalStorage } from '@mantine/hooks'
 import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core'
 import Layout from '../components/Layout'
 import '../styles/globals.scss'
 
 const App = ({ Component, pageProps }: AppProps) => {
-    // Set theme and detect the user's preference (dark or light)
+    // Detect the user's theme preference (dark or light) and save it
     const preferredColorScheme = useColorScheme()
-    const [colorScheme, setColorScheme] = useState<ColorScheme>(preferredColorScheme)
+    const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+        key: 'mantine-color-scheme',
+        defaultValue: preferredColorScheme,
+    })
     const toggleColorScheme = (value?: ColorScheme) =>
         setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
 
