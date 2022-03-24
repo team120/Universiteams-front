8c6911e07489360db878a22a9b7474e5cb864ff7
Nicolás Antonelli <niconelli2@gmail.com>
2022-03-24 01:02:01 -0300

    Theme: fix hook invalid declaration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,13 +6,13 @@ import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core
 import Layout from '../components/Layout'
 import '../styles/globals.scss'
 
-// Set theme and detect the user's preference (dark or light)
-const preferredColorScheme = useColorScheme()
-const [colorScheme, setColorScheme] = useState<ColorScheme>(preferredColorScheme)
-const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
-
 const App = ({ Component, pageProps }: AppProps) => {
+    // Set theme and detect the user's preference (dark or light)
+    const preferredColorScheme = useColorScheme()
+    const [colorScheme, setColorScheme] = useState<ColorScheme>(preferredColorScheme)
+    const toggleColorScheme = (value?: ColorScheme) =>
+        setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
+
     return (
         <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
             <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
