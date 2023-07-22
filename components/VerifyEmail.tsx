3f25bc5db77d488df48dce202e40cea0d981f25f
Alejandro Recalde <alejandrorecalde5@gmail.com>
2023-07-22 20:28:49 -0300

    Add missing "import React from 'react'"

--- components/VerifyEmail.tsx
+++ components/VerifyEmail.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { Loader, Card, Text, Button } from "@mantine/core";
 import axios from "axios";
--- pages/account/verify.tsx
+++ pages/account/verify.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import VerifyEmail from '../../components/VerifyEmail'
 
 const VerifyAccountPage = () => {
