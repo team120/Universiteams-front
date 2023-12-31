194eaca396249140641ec2d12901305b281be5f2
NicoAntonelli <niconelli2@gmail.com>
2023-12-31 17:30:53 +0100

    Extract theme as own file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,48 +3,15 @@ import React from 'react'
 import Head from 'next/head'
 import axios from 'axios'
 
+import { MantineProvider, localStorageColorSchemeManager } from '@mantine/core'
 import '@mantine/core/styles.css'
-import {
-  MantineProvider,
-  createTheme,
-  localStorageColorSchemeManager,
-  useMantineColorScheme,
-  useComputedColorScheme,
-} from '@mantine/core'
-import { useHotkeys } from '@mantine/hooks'
 import '@/styles/globals.scss'
+import Theme from './theme'
 import Layout from '@/components/Layout'
 
 const App = ({ children, pageProps }: any) => {
   // Detect the user's theme preference (dark or light)
   const colorSchemeManager = localStorageColorSchemeManager({ key: 'mantine-color-scheme' })
-
-  // Mantine theme
-  const theme = createTheme({
-    primaryColor: 'blue',
-    colors: {
-      orange: [
-        '#fef2e7',
-        '#fbd8b7',
-        '#f8bf87',
-        '#f5a557',
-        '#f28b27',
-        '#d8720d',
-        '#a8580a',
-        '#783f07',
-        '#482604',
-        '#180d01',
-      ],
-    },
-    shadows: {
-      md: '1px 1px 3px rgba(0, 0, 0, .25)',
-      xl: '5px 5px 3px rgba(0, 0, 0, .25)',
-    },
-    fontFamily: 'Verdana, sans-serif',
-    fontFamilyMonospace: 'Monaco, Courier, monospace',
-    headings: { fontFamily: 'Greycliff CF, sans-serif' },
-    spacing: { xs: '15', sm: '20', md: '25', lg: '30', xl: '40' },
-  })
 
   // Global configs
   axios.defaults.withCredentials = true
@@ -58,7 +25,7 @@ const App = ({ children, pageProps }: any) => {
       </Head>
       <body>
         <MantineProvider
-          theme={theme}
+          theme={Theme}
           colorSchemeManager={colorSchemeManager}
           defaultColorScheme="dark">
           <Layout>
