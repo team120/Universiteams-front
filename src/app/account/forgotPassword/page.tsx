ba4a78cef29ba5e6e51cd395234f500a5af50711
NicoAntonelli <niconelli2@gmail.com>
2023-12-31 13:01:00 +0100

    Set every page as client side

--- src/app/account/forgotPassword/page.tsx
+++ src/app/account/forgotPassword/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import { NextPage } from 'next'
 import ForgotPasswordComponent from '@/components/ForgotPassword'
--- src/app/account/resetPassword/page.tsx
+++ src/app/account/resetPassword/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import PasswordReset from '@/components/PasswordReset'
 
