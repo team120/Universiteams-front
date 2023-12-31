2ec61d0dd7d736330aa40d98d3d37fc321e57ccd
NicoAntonelli <niconelli2@gmail.com>
2023-12-31 15:24:13 +0100

    Migrate mantine theme to v7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
 'use client'
 import React from 'react'
-import axios from 'axios'
 import Head from 'next/head'
+import axios from 'axios'
 
-// import type { NextPage } from 'next'
-// import type { AppProps } from 'next/app'
-
-import { useColorScheme, useHotkeys, useLocalStorage } from '@mantine/hooks'
-import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css'
+import {
+  MantineProvider,
+  createTheme,
+  localStorageColorSchemeManager,
+  useMantineColorScheme,
+  useComputedColorScheme,
+} from '@mantine/core'
+import { useHotkeys } from '@mantine/hooks'
 import '@/styles/globals.scss'
 import Layout from '@/components/Layout'
 
@@ -17,24 +21,40 @@ interface App {
 }
 
 const App = ({ children, pageProps }: App) => {
-  // Detect the user's theme preference (dark or light) and save it
-  const preferredColorScheme = useColorScheme()
-  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
-    key: 'mantine-color-scheme',
-    defaultValue: preferredColorScheme,
-  })
-  const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
-  const themeConfig: any = {
-    colorScheme,
+  // Detect the user's theme preference (dark or light)
+  const colorSchemeManager = localStorageColorSchemeManager({ key: 'mantine-color-scheme' })
+
+  // Change between theme preferences
+  const { setColorScheme } = useMantineColorScheme()
+  const computedColorScheme = useComputedColorScheme('dark')
+  const toggleColorScheme = () => setColorScheme(computedColorScheme === 'dark' ? 'light' : 'dark')
+
+  // Mantine theme
+  const theme = createTheme({
     primaryColor: 'blue',
-    secondaryColor: 'orange',
+    colors: {
+      orange: [
+        '#fef2e7',
+        '#fbd8b7',
+        '#f8bf87',
+        '#f5a557',
+        '#f28b27',
+        '#d8720d',
+        '#a8580a',
+        '#783f07',
+        '#482604',
+        '#180d01',
+      ],
+    },
+    shadows: {
+      md: '1px 1px 3px rgba(0, 0, 0, .25)',
+      xl: '5px 5px 3px rgba(0, 0, 0, .25)',
+    },
     fontFamily: 'Verdana, sans-serif',
     fontFamilyMonospace: 'Monaco, Courier, monospace',
     headings: { fontFamily: 'Greycliff CF, sans-serif' },
-    spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
-    loader: 'bars',
-  }
+    spacing: { xs: '15', sm: '20', md: '25', lg: '30', xl: '40' },
+  })
 
   // Hotkeys
   useHotkeys([['mod+J', () => toggleColorScheme()]])
@@ -50,14 +70,15 @@ const App = ({ children, pageProps }: App) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
-        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-          <MantineProvider theme={themeConfig} withGlobalStyles withNormalizeCSS>
-            <Layout>
-              {children}
-              {/* <Component {...pageProps} /> */}
-            </Layout>
-          </MantineProvider>
-        </ColorSchemeProvider>
+        <MantineProvider
+          theme={theme}
+          colorSchemeManager={colorSchemeManager}
+          defaultColorScheme="dark">
+          <Layout>
+            {children}
+            {/* <Component {...pageProps} /> */}
+          </Layout>
+        </MantineProvider>
       </body>
     </html>
   )
