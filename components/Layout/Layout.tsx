c377e09bd94957887c1db937a8ddedcbe009cc7f
NicoAntonelli <niconelli2@gmail.com>
2024-01-03 03:34:02 +0100

    Layout: fix size

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -29,10 +29,10 @@ const Layout = (props: Layout) => {
   return (
     <AppShell
       header={{ height: 60 }}
-      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+      navbar={{ width: { sm: 300, lg: 400 }, breakpoint: 'sm', collapsed: { mobile: !opened } }}
       padding="md">
       <AppShell.Header>
-        <Group h="100%" px="md">
+        <Group ml={10} h="100%" px="md">
           <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
         </Group>
       </AppShell.Header>
