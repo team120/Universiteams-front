e0d4b89d09c6cbe0576e6842fc586191607de0f9
NicoAntonelli <niconelli2@gmail.com>
2024-01-03 03:08:24 +0100

    Add NavbarItem component

---
+++ components/Layout/Navbar/NavbarItem.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Box, Container, Flex } from '@mantine/core'
+
+interface INavbarItem {
+  text: string
+  textSecondLine?: string
+  link?: string
+  icon?: JSX.Element
+  small?: boolean
+}
+
+const NavbarItem = (props: INavbarItem) => {
+  return (
+    <Flex
+      h={props.small ? 45 : 60}
+      bg="var(--mantine-color-gray-light)"
+      pl={20}
+      align={'center'}
+      gap={20}>
+      {props.icon && props.icon}
+      {props.textSecondLine ? (
+        <Box>
+          <Container>{props.text}</Container>
+          <Container>{props.textSecondLine}</Container>
+        </Box>
+      ) : (
+        <Box>
+          <Container>{props.text}</Container>
+        </Box>
+      )}
+    </Flex>
+  )
+}
+
+export default NavbarItem
