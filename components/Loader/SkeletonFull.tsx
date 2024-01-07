b0db921ffce03302596517aac7af2418e57fe8a8
NicoAntonelli <niconelli2@gmail.com>
2024-01-08 00:49:44 +0100

    Add skeleton components

---
+++ components/Loader/SkeletonFull.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Skeleton } from '@mantine/core'
+
+const SkeletonFull = () => {
+  return (
+    <>
+      <Skeleton height={8} radius="xl" />
+      <Skeleton height={8} mt={6} radius="xl" />
+      <Skeleton height={8} mt={6} width="70%" radius="xl" />
+      <Skeleton height={8} mt={6} width="70%" radius="xl" />
+      <br />
+      <Skeleton height={8} radius="xl" />
+      <Skeleton height={8} mt={6} radius="xl" />
+      <Skeleton height={8} mt={6} width="70%" radius="xl" />
+      <Skeleton height={8} mt={6} width="70%" radius="xl" />
+      <br />
+      <Skeleton height={8} radius="xl" />
+      <Skeleton height={8} mt={6} radius="xl" />
+      <Skeleton height={8} mt={6} width="70%" radius="xl" />
+      <Skeleton height={8} mt={6} width="70%" radius="xl" />
+      <br />
+      <Skeleton height={8} radius="xl" />
+      <Skeleton height={8} mt={6} radius="xl" />
+      <Skeleton height={8} mt={6} width="70%" radius="xl" />
+      <Skeleton height={8} mt={6} width="70%" radius="xl" />
+    </>
+  )
+}
+
+export default SkeletonFull
