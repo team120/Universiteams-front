fcd921a99b3947cdaefada4f754e6552a35bd35c
NicoAntonelli <niconelli2@gmail.com>
2024-01-08 01:55:26 +0100

    Add special pages: not-found, error & global-error

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+'use client'
+import React from 'react'
+import ErrorNotFound from '@/components/Error/ErrorNotFound'
+
+const NotFound = () => {
+  return <ErrorNotFound />
+}
+
+export default NotFound
