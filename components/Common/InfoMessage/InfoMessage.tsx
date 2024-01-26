93bb48822429c5ba13312e46dff42f80b03af395
NicoAntonelli <niconelli2@gmail.com>
2024-01-26 20:47:12 +0100

    Add InfoMessage component

---
+++ components/Common/InfoMessage/InfoMessage.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import MessageType from '@/entities/HelpTypes/MessageType'
+import { IconAlertOctagon, IconAlertTriangle, IconInfoCircle } from '@tabler/icons-react'
+import { Flex, Text } from '@mantine/core'
+
+interface MessageProps {
+  text: string
+  type?: MessageType
+}
+
+const Message = (props: MessageProps) => {
+  const setIcon = (): JSX.Element => {
+    const type = props.type ?? 'info'
+    switch (type) {
+      case 'error':
+        return <IconAlertOctagon color="red" size={'2.5rem'} />
+      case 'warning':
+        return <IconAlertTriangle color="yellow" size={'2.5rem'} />
+      case 'info':
+      default:
+        return <IconInfoCircle color="lightgreen" size={'2.5rem'} />
+    }
+  }
+
+  const icon = setIcon()
+
+  return (
+    <Flex m={20} p={20} bg={'dark'} align={'center'}>
+      {icon}{' '}
+      <Text ml={10} size="1.2rem">
+        {props.text}
+      </Text>
+    </Flex>
+  )
+}
+
+export default Message
