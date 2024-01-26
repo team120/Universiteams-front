cf93597c07bbf3f76996ac538135fa507c5a0634
NicoAntonelli <niconelli2@gmail.com>
2024-01-26 20:48:00 +0100

    Extract project item as own component, use loader and suspense

---
+++ components/Project/ProjectItem.tsx
@@ -0,0 +1,69 @@
+import React, { Suspense } from 'react'
+import { Badge, Card, Chip, Group, Text, useMantineTheme } from '@mantine/core'
+import Dates from 'utils/string/Dates'
+import Project from '@/entities/Project'
+import InfoMessage from '../Common/InfoMessage/InfoMessage'
+import SkeletonSmall from '../Loader/SkeletonSmall'
+
+interface ProjectItemProps {
+  project?: Project
+}
+
+const ProjectItem = (props: ProjectItemProps) => {
+  const theme = useMantineTheme()
+  const project = props.project
+
+  // Small loader needed?
+  if (!project) return <InfoMessage text="No se ha podido cargar el projecto" type="error" />
+
+  return (
+    <Suspense fallback={<SkeletonSmall />}>
+      <Card
+        key={project.id}
+        padding="lg"
+        radius="md"
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          width: '100%',
+          borderStyle: 'solid',
+          borderColor: 'gray',
+        }}>
+        <div style={{ width: '100%' }}>
+          <Card.Section>
+            <Text style={{ fontSize: '1.25rem', fontWeight: 500, lineHeight: '1.75rem' }}>
+              {project.name}
+            </Text>
+            <Group justify="space-between" style={{ marginBottom: theme.spacing.xs }}>
+              <Text style={{ fontWeight: 500 }}>
+                {project.type} | {Dates.formatDate(project.creationDate)}
+                {project.endDate ? ' - ' + Dates.formatDate(project.endDate) : ''}
+              </Text>
+              <Badge color="pink" variant="light">
+                {project.researchDepartments[0].name}
+              </Badge>
+            </Group>
+          </Card.Section>
+
+          <Chip.Group>
+            <Group style={{ marginTop: theme.spacing.xs }}>
+              {project.enrollments && (
+                <Chip variant="light" color="blue" size="md">
+                  {project.enrollments[0].user.firstName} {project.enrollments[0].user.lastName}, +
+                  {project.userCount} personas
+                </Chip>
+              )}
+              {project.interests.map((interest) => (
+                <Chip variant="light" key={interest.id} color="blue" size="md">
+                  {interest.name}
+                </Chip>
+              ))}
+            </Group>
+          </Chip.Group>
+        </div>
+      </Card>
+    </Suspense>
+  )
+}
+
+export default ProjectItem
--- components/Project/ProjectsList.tsx
+++ components/Project/ProjectsList.tsx
@@ -1,72 +1,31 @@
-import React from 'react'
-import { Card, Text, Badge, useMantineTheme, Group, List, Chip } from '@mantine/core'
+import React, { Suspense } from 'react'
+import { List } from '@mantine/core'
 import { useMediaQuery } from '@mantine/hooks'
 
-import Dates from 'utils/string/Dates'
 import Theme from 'src/app/theme'
 import Project from '@/entities/Project'
+import ProjectItem from './ProjectItem'
+import SkeletonFull from '../Loader/SkeletonFull'
+import InfoMessage from '../Common/InfoMessage/InfoMessage'
 
 interface ProjectsListProps {
   projects?: Project[]
 }
 
-function ProjectsList({ projects }: ProjectsListProps) {
-  const theme = useMantineTheme()
-
+const ProjectsList = ({ projects }: ProjectsListProps) => {
   const isMobile = useMediaQuery('(max-width: 768px)')
 
-  return (
-    <>
-      <List ml={!isMobile ? Theme.spacing?.xs : 0}>
-        {(!projects || projects.length == 0) && <p>No hay projects</p>}
-        {projects?.map((project) => (
-          <Card
-            key={project.id}
-            padding="lg"
-            radius="md"
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              width: '100%',
-              borderStyle: 'solid',
-              borderColor: 'gray',
-            }}>
-            <div style={{ width: '100%' }}>
-              <Card.Section>
-                <Text style={{ fontSize: '1.25rem', fontWeight: 500, lineHeight: '1.75rem' }}>
-                  {project.name}
-                </Text>
-                <Group justify="space-between" style={{ marginBottom: theme.spacing.xs }}>
-                  <Text style={{ fontWeight: 500 }}>
-                    {project.type} | {Dates.formatDate(project.creationDate)}
-                    {project.endDate ? ' - ' + Dates.formatDate(project.endDate) : ''}
-                  </Text>
-                  <Badge color="pink" variant="light">
-                    {project.researchDepartments[0].name}
-                  </Badge>
-                </Group>
-              </Card.Section>
+  if (!projects) return <SkeletonFull />
+  if (projects.length >= 0) return <InfoMessage text="Aún no tienes projectos" type="info" />
 
-              <Chip.Group>
-                <Group style={{ marginTop: theme.spacing.xs }}>
-                  {project.enrollments && (
-                    <Chip variant="light" color="blue" size="md">
-                      {project.enrollments[0].user.firstName} {project.enrollments[0].user.lastName}
-                      , +{project.userCount} personas
-                    </Chip>
-                  )}
-                  {project.interests.map((interest) => (
-                    <Chip variant="light" key={interest.id} color="blue" size="md">
-                      {interest.name}
-                    </Chip>
-                  ))}
-                </Group>
-              </Chip.Group>
-            </div>
-          </Card>
+  return (
+    <Suspense fallback={<SkeletonFull />}>
+      <List ml={!isMobile ? Theme.spacing?.xs : 0}>
+        {projects.map((project) => (
+          <ProjectItem project={project} />
         ))}
       </List>
-    </>
+    </Suspense>
   )
 }
 
