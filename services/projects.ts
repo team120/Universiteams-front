3d6ff5f9df786c768151670f9752ae7389eb426d
NicoAntonelli <niconelli2@gmail.com>
2024-01-26 18:45:22 +0100

    Create projects service

---
+++ services/projects.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+import Project from '@/entities/Project'
+
+const backendAPI = process.env.NEXT_PUBLIC_BACKEND_HOST
+const prefix = `${backendAPI}/projects`
+
+// Find all projects
+const GetProjects = async (): Promise<Project[] | undefined> => {
+  try {
+    const url = `${prefix}/`
+    const result = await axios.get(url)
+    return result.data
+  } catch (error) {
+    console.log(error)
+    return undefined
+  }
+}
+
+export const Projects = { GetProjects }
