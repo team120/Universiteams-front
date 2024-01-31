b875af82faab3096448d411896fd93351f28245f
Alejandro Recalde <alejandrorecalde5@gmail.com>
2024-01-31 00:45:07 -0300

    Make api request using project filters in url query

--- services/projects.ts
+++ services/projects.ts
@@ -1,15 +1,39 @@
 import axios from 'axios'
 import Env from 'utils/config/Env'
 
-import Project from '@/entities/Project'
 import ProjectsResult from '@/entities/ProjectsResult'
 
 const prefix = `${Env.backendAPI}/projects`
 
+export interface GetProjectsInput {
+  generalSearchTerm?: string
+  universityId?: number
+  departmentId?: number
+  type?: string
+  dateFrom?: Date
+  isDown?: boolean
+  sortBy?: string
+  inAscendingOrder?: boolean
+}
+
 // Find all projects
-const GetProjects = async (): Promise<ProjectsResult | undefined> => {
+const GetProjects = async (params?: GetProjectsInput): Promise<ProjectsResult | undefined> => {
   try {
-    const url = `${prefix}/`
+    const url = `${prefix}`
+      .concat(params ? '?' : '')
+      .concat(params?.generalSearchTerm ? `generalSearch=${params.generalSearchTerm}&` : '')
+      .concat(params?.universityId ? `universityId=${params.universityId}&` : '')
+      .concat(params?.departmentId ? `departmentId=${params.departmentId}&` : '')
+      .concat(params?.type ? `type=${params.type}&` : '')
+      .concat(params?.dateFrom ? `dateFrom=${params.dateFrom}&` : '')
+      .concat(params?.isDown ? `isDown=${params.isDown}&` : '')
+      .concat(
+        params?.sortBy
+          ? `sortBy=${params.sortBy}&inAscendingOrder=${params?.inAscendingOrder ?? true}&`
+          : ''
+      )
+
+    console.log(url)
     const result = await axios.get<ProjectsResult>(url)
     return result.data
   } catch (error) {
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,13 +2,14 @@
 import React, { useState, useEffect } from 'react'
 import { NextPage } from 'next'
 
-import { Projects } from '@/services/projects'
+import { GetProjectsInput, Projects } from '@/services/projects'
 import ProjectsResult from '@/entities/ProjectsResult'
 import SelectItem from '@/entities/HelpTypes/SelectItem'
 
 import Filter from '@/components/Filter'
 import ProjectFilterContent from '@/components/Project/ProjectFilterContent'
 import ProjectsList from '@/components/Project/ProjectsList'
+import { useSearchParams } from 'next/navigation'
 
 const ProjectsPage: NextPage = () => {
   const [projectsResult, setProjectsResult] = useState<ProjectsResult>()
@@ -20,18 +21,39 @@ const ProjectsPage: NextPage = () => {
     { attribute: 'researchDepartment', displayName: 'researchDepartment' },
   ]
 
-  const getProjects = async () => {
-    const result = await Projects.GetProjects()
-    if (!result) {
-      setProjectsResult(undefined)
-      return
-    }
+  const searchQuery = useSearchParams()
+
+  const getProjects = async (params: GetProjectsInput) => {
+    const result = await Projects.GetProjects(params)
     setProjectsResult(result)
   }
 
   useEffect(() => {
-    getProjects()
-  }, [])
+    getProjects({
+      generalSearchTerm: searchQuery.get('generalSearch') || undefined,
+      universityId: searchQuery.get('university')
+        ? parseInt(searchQuery.get('university')!)
+        : undefined,
+      departmentId: searchQuery.get('department')
+        ? parseInt(searchQuery.get('department')!)
+        : undefined,
+      type: searchQuery.get('type') || undefined,
+      isDown:
+        searchQuery.get('isDown') === 'true'
+          ? true
+          : searchQuery.get('isDown') === 'false'
+          ? false
+          : undefined,
+      dateFrom: searchQuery.get('dateFrom') ? new Date(searchQuery.get('dateFrom')!) : undefined,
+      sortBy: searchQuery.get('sortBy') || undefined,
+      inAscendingOrder:
+        searchQuery.get('inAscendingOrder') === 'true'
+          ? true
+          : searchQuery.get('inAscendingOrder') === 'false'
+          ? false
+          : undefined,
+    })
+  }, [searchQuery])
 
   return (
     <>
