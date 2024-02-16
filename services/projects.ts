9ef9046be3e21bf423647e052c8a0223cf67d726
alereca <alereca@users.noreply.github.com>
2024-02-16 04:50:10 +0000

    Apply eslint and prettier code changes

--- services/projects.ts
+++ services/projects.ts
@@ -27,8 +27,14 @@ const GetProjects = async (params?: GetProjectsInput): Promise<ProjectsResult | 
       .concat(params?.generalSearchTerm ? `generalSearch=${params.generalSearchTerm}&` : '')
       .concat(params?.institutionId ? `institutionId=${params.institutionId}&` : '')
       .concat(params?.facilityId ? `facilityId=${params.facilityId}&` : '')
-      .concat(params?.researchDepartmentId ? `researchDepartmentId=${params.researchDepartmentId}&` : '')
-      .concat(params?.interestIds && params?.interestIds.length > 0 ? `interestIds=${params.interestIds}&` : '')
+      .concat(
+        params?.researchDepartmentId ? `researchDepartmentId=${params.researchDepartmentId}&` : ''
+      )
+      .concat(
+        params?.interestIds && params?.interestIds.length > 0
+          ? `interestIds=${params.interestIds}&`
+          : ''
+      )
       .concat(params?.userId ? `userId=${params.userId}&` : '')
       .concat(params?.type ? `type=${params.type}&` : '')
       .concat(params?.dateFrom ? `dateFrom=${params.dateFrom.toISOString()}&` : '')
