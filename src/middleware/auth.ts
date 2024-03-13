239fa2c7ebaa4b7b74c6fc911ecd16cf8d68b558
NicoAntonelli <niconelli2@gmail.com>
2024-03-13 19:53:17 -0300

    Middleware: add Auth middleware for JWT verification

---
+++ src/middleware/auth.ts
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+
+// Verify token signature (validation) and return original values
+const verifyToken = (token: string) => {
+  if (!token) return null
+
+  // Erase bearer part
+  if (token.includes('Bearer%20')) token = token.split('%20')[1] || ''
+  if (!token) return null
+
+  const SECRET_KEY = process.env.JWT_KEY || ''
+
+  try {
+    const key = Buffer.from(SECRET_KEY, 'base64')
+    return jwt.verify(token, key)
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
+
+export default verifyToken
