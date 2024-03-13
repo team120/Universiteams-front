673721f61ad02a9d60fef4c84955abcf7c55d72c
NicoAntonelli <niconelli2@gmail.com>
2024-03-13 19:55:26 -0300

    Middleware: whitelist, redirection & JWT validation

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+import verifyToken from './middleware/auth'
+import AccessJWTPayload from '@/entities/HelpTypes/AccessJWTPayload'
+
 export const config = {
   matcher: [
     {
@@ -13,4 +16,23 @@ export const config = {
       ],
     },
   ],
+}
+
+export const middleware = (request: NextRequest) => {
+  const pathName: string = request.nextUrl.pathname ?? ''
+  const redirection: URL = new URL('/account/login', request.url)
+
+  // Whitelist for auth
+  const whitelist = ['/account/login', 'account/register']
+  if (whitelist.includes(pathName)) return
+
+  // Auth validation - Access token needed
+  let token: string = request.cookies.get('accessToken')?.value ?? ''
+  if (!token) return NextResponse.redirect(redirection)
+
+  // Auth validation - Must be a valid JWT and have private signature
+  const payload: AccessJWTPayload | null = verifyToken(token) as AccessJWTPayload | null
+  if (!payload) return NextResponse.redirect(redirection)
+
+  return
 }
