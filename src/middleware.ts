a6b397de8e2e10f7c0a38e71926cb3736dac621c
NicoAntonelli <niconelli2@gmail.com>
2024-03-13 21:35:37 -0300

    Middleware: comment content temporary

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
+
 import type { NextRequest } from 'next/server'
+import { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies'
 
 import verifyToken from './middleware/auth'
 import AccessJWTPayload from '@/entities/HelpTypes/AccessJWTPayload'
@@ -22,10 +25,16 @@ export const middleware = (request: NextRequest) => {
   const pathName: string = request.nextUrl.pathname ?? ''
   const redirection: URL = new URL('/account/login', request.url)
 
-  // Whitelist for auth
-  const whitelist = ['/account/login', 'account/register']
-  if (whitelist.includes(pathName)) return
+  // NextResponse.cookies.set('testHTTP', 'test', options)
+  // console.log(cookies)
+  // console.log(cookies())
+  // console.log(request.cookies.getAll())
 
+  // Whitelist for auth
+  const whitelist = ['/account/login', '/account/register']
+  if (whitelist.includes(pathName)) return NextResponse.next()
+
+  /*
   // Auth validation - Access token needed
   let token: string = request.cookies.get('accessToken')?.value ?? ''
   if (!token) return NextResponse.redirect(redirection)
@@ -33,6 +42,7 @@ export const middleware = (request: NextRequest) => {
   // Auth validation - Must be a valid JWT and have private signature
   const payload: AccessJWTPayload | null = verifyToken(token) as AccessJWTPayload | null
   if (!payload) return NextResponse.redirect(redirection)
+  */
 
-  return
+  return NextResponse.next()
 }
