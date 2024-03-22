2416a1f45754f69be894a7d3f7df44331e56dcc1
Alejandro Recalde <alejandrorecalde5@gmail.com>
2024-03-21 23:50:40 -0300

    Fix route paths in Constants.ts

--- utils/string/Constants.ts
+++ utils/string/Constants.ts
@@ -3,11 +3,11 @@
 // Routes that don't need auth
 // Also don't have navigation and search bars
 const noAuthRoutes = [
-  'account/forgotPassword',
-  'account/login',
-  'account/register',
-  'account/resetpassword',
-  'account/verifyEmail',
+  '/account/forgotPassword',
+  '/account/login',
+  '/account/register',
+  '/account/resetpassword',
+  '/account/verifyEmail',
 ]
 
 const Constants = { noAuthRoutes }
