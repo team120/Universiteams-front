0b63be932d45b40f5da1c1f959bb7f70a08e88f9
Alejandro Recalde <alejandrorecalde5@gmail.com>
2024-03-22 00:55:18 -0300

    Fix import statement in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import axios from 'axios'
 
-import { ColorSchemeScript, MantineProvider, localStorageColorSchemeManager } from '@mantine/core'
+import { MantineProvider, localStorageColorSchemeManager } from '@mantine/core'
 import '@mantine/core/styles.css'
 import '@mantine/dates/styles.css'
 import '@/styles/globals.scss'
