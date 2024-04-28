708d940f00aa5283fd18be081b2e24b5e265d245
Alejandro Recalde <alejandrorecalde5@gmail.com>
2024-04-28 03:53:54 -0300

    Fix missing checks or crosses in register form password validation

--- components/Account/Requirement.tsx
+++ components/Account/Requirement.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Flex, Text } from '@mantine/core'
+import { Flex, Text, useMantineTheme } from '@mantine/core'
 import { IconCheck, IconX } from '@tabler/icons-react'
 
 const Requirement = ({ meets, label }: { meets: boolean; label: string }) => {
+  const theme = useMantineTheme()
+
   return (
     <Flex align="center" mt={5}>
-      {meets ? <IconCheck size={14} color="teal.6" /> : <IconX size={14} color="red.8" />}
+      {meets ? (
+        <IconCheck size={14} color={theme.colors.teal[6]} />
+      ) : (
+        <IconX size={14} color={theme.colors.red[8]} />
+      )}
       <Text ml={7} size="sm" c={meets ? 'teal.6' : 'red.8'}>
         {label}
       </Text>
