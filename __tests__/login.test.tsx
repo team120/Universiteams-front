5f7647dd4508d904c296902163791b68c56bd8ab
Alejandro Recalde <alejandrorecalde5@gmail.com>
2024-04-29 21:06:07 -0300

    refactor: Add react-query to login test

--- __tests__/login.test.tsx
+++ __tests__/login.test.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { render, fireEvent } from '../utils/tests'
 import '@testing-library/jest-dom/extend-expect' // Import the matchers
 import Login from 'src/app/account/login/page'
@@ -6,7 +7,13 @@ jest.mock('next/navigation', () => require('next-router-mock'))
 
 describe('Login page', () => {
   it('moves to register form if login button is clicked', () => {
-    const { getByText } = render(<Login />)
+    const queryClient = new QueryClient()
+
+    const { getByText } = render(
+      <QueryClientProvider client={queryClient}>
+        <Login />
+      </QueryClientProvider>
+    )
     const registerLink = getByText('Registrarse')
 
     fireEvent.click(registerLink)
