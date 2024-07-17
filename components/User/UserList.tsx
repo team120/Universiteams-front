787817ff7119121a23d88d44a6dd6e8fae4d3152
NicoAntonelli <niconelli2@gmail.com>
2024-07-17 01:44:03 -0300

    Users: filter & list components, small css module

---
+++ components/User/UserList.tsx
@@ -0,0 +1,33 @@
+import React, { Suspense } from 'react'
+import { List } from '@mantine/core'
+import { useMediaQuery } from '@mantine/hooks'
+import Theme from 'src/app/theme'
+
+import InfoMessage from '../Common/InfoMessage/InfoMessage'
+import SkeletonFull from '../Loader/SkeletonFull'
+import User from '@/entities/User'
+import UserItem from './UserItem'
+
+interface UserListProps {
+  //users?: UserInList[]
+  users?: any // Fix type
+}
+
+const UsersList = ({ users }: UserListProps) => {
+  const isMobile = useMediaQuery(`(max-width: ${Theme.breakpoints?.lg})`)
+
+  if (!users) return <SkeletonFull />
+  if (users.length == 0) return <InfoMessage text="No hay usuarios para mostrar" type="info" />
+
+  return (
+    <Suspense fallback={<SkeletonFull />}>
+      <List ml={!isMobile ? 'xs' : 0}>
+        {users.map((user: User) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </List>
+    </Suspense>
+  )
+}
+
+export default UsersList
