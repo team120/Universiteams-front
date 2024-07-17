fbaffc15d867455f068ec34e5d94febb9ac0281c
NicoAntonelli <niconelli2@gmail.com>
2024-07-17 01:42:00 -0300

    Users: rename service, add find all and get one

---
+++ services/users.ts
@@ -0,0 +1,42 @@
+import axios from 'axios'
+import Env from 'utils/config/Env'
+import { keepPreviousData, queryOptions } from '@tanstack/react-query'
+
+import { EnrollmentRequestsShow } from '@/entities/HelpTypes/EnrollmentRequestsShow'
+import User from '@/entities/User'
+import { UserSortAttribute } from '@/entities/UserInList'
+
+const prefix = `${Env.backendAPI}/users`
+
+export interface GetUsersInput {
+  generalSearchTerm?: string
+  // firstName, lastName, email
+  interestIds?: number[]
+  userId?: number
+  sortBy?: UserSortAttribute
+  inAscendingOrder?: boolean
+  limit?: number
+  offset?: number
+}
+
+export const Users = {
+  // Find all users
+  getUsers: async (params?: GetUsersInput) => {
+    const url = `${prefix}`
+    // To-do: use params to filter the results (general search and/or filters)
+    const result = await axios.get<User[]>(url)
+    return result.data
+  },
+
+  // Get one user by id
+  getUser: async (id: number): Promise<User | undefined> => {
+    const result = await axios.get<User>(`${prefix}/${id}`)
+    return result.data
+  },
+
+  // Get the enrollments of a user
+  async getEnrollmentRequests(id: number): Promise<EnrollmentRequestsShow> {
+    const result = await axios.get<EnrollmentRequestsShow>(`${prefix}/${id}/enroll-requests`)
+    return result.data
+  },
+}
