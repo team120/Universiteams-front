35367c30cea429a34f53ca433468275c692ab67a
NicoAntonelli <niconelli2@gmail.com>
2024-08-10 18:13:52 -0300

    Skeleton components: fix margin

--- components/Loader/SkeletonSmall.tsx
+++ components/Loader/SkeletonSmall.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Skeleton } from '@mantine/core'
+import { Box, Skeleton } from '@mantine/core'
 
 const SkeletonSmall = () => {
   return (
-    <>
+    <Box mt={6} mx={20}>
       <Skeleton height={8} mt={6} radius="xl" />
       <Skeleton height={8} mt={6} radius="xl" />
       <Skeleton height={8} mt={6} width="70%" radius="xl" />
       <Skeleton height={8} mt={6} width="70%" radius="xl" />
-    </>
+    </Box>
   )
 }
 
