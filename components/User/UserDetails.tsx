c07cb56421d1e32ba406e7901e4884d28c517571
NicoAntonelli <niconelli2@gmail.com>
2024-08-11 03:37:47 -0300

    Users: enhancement for details component, use tabs

--- components/User/UserDetails.tsx
+++ components/User/UserDetails.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
-import { Alert, Badge, Card, Chip, Group, Text } from '@mantine/core'
+import { Alert, Badge, Card, Chip, Flex, Group, Loader, Tabs, Text } from '@mantine/core'
 
 import { CurrentUserQueryOptions } from '@/services/currentUser'
 import { Users } from '@/services/users'
 
 import SkeletonFull from '@/components/Loader/SkeletonFull'
+import { IconFolders, IconMail, IconSend, IconUser } from '@tabler/icons-react'
+import { EnrollmentList } from '../Enrollment/EnrollmentList'
+import { EnrollmentRequestShow } from '@/entities/HelpTypes/EnrollmentRequestShow'
+import UserAffiliation from '@/entities/UserAffiliation'
+import Interest from '@/entities/Interest'
 
 interface UserDetailsParams {
   id: number
 }
 
 export enum UserDetailsTabs {
-  Users = 'users',
+  Projects = 'projects',
+  Requests = 'requests',
 }
 
 const UserDetails = (props: UserDetailsParams) => {
@@ -26,11 +32,10 @@ const UserDetails = (props: UserDetailsParams) => {
     queryFn: () => Users.getUser(props.id),
   })
 
-  const queryClient = useQueryClient()
-
-  const { data: currentUser, error: errorCurrentUser } = useQuery(
-    CurrentUserQueryOptions.currentUser()
-  )
+  // No mutations --- Current used not needed now
+  // const { data: currentUser, error: errorCurrentUser } = useQuery(
+  //   CurrentUserQueryOptions.currentUser()
+  // )
 
   const {
     data: enrollmentRequests,
@@ -45,8 +50,20 @@ const UserDetails = (props: UserDetailsParams) => {
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
+  // Handle methods in users endpoint
   const handleInterestTagClick = (interestId: number) => {
     router.push(`/users?interest=${interestId}`)
+  }
+
+  // Handle method in projects endpoint
+  const handleDepartmentBadgeClick = (
+    institutionId: number,
+    facilityId: number,
+    departmentId: number
+  ) => {
+    router.push(
+      `/projects?university=${institutionId}&facility=${facilityId}&department=${departmentId}`
+    )
   }
 
   if (isLoading) return <SkeletonFull />
@@ -56,12 +73,23 @@ const UserDetails = (props: UserDetailsParams) => {
   return (
     <>
       <Card key={user.id} mx="md" mb="0.5rem" p="md" radius="md" withBorder>
-        <Text style={{ fontSize: '1.25rem', fontWeight: 500, lineHeight: '1.75rem' }}>
+        <Text style={{ fontSize: '3rem', fontWeight: 500, lineHeight: '3rem' }}>
           {user.firstName} {user.lastName}
         </Text>
+        <Flex direction={'row'} align={'center'} gap={10} mt="0.5rem">
+          <IconMail size={25} />
+          <Text
+            style={{
+              fontSize: '1.25rem',
+              fontWeight: 500,
+              lineHeight: '2rem',
+            }}>
+            {user.email}
+          </Text>
+        </Flex>
 
         <Chip.Group>
-          <Group gap={'0.5rem'} mt={'1rem'}>
+          <Group gap={'0.5rem'} my={'1rem'}>
             {user.interests.map((interest) => (
               <Badge
                 variant="dot"
@@ -76,7 +104,102 @@ const UserDetails = (props: UserDetailsParams) => {
           </Group>
         </Chip.Group>
 
-        {/* To-Do: Enrollments & more info */}
+        <Tabs
+          value={searchParams.get('activeTab') ?? UserDetailsTabs.Projects}
+          onChange={(value) => router.push(`${pathname}?activeTab=${value}`)}>
+          <Tabs.List>
+            <Tabs.Tab
+              value={UserDetailsTabs.Projects}
+              leftSection={<IconFolders />}
+              rightSection={
+                <Badge color="blue" variant="filled">
+                  {/* user.enrollmentCount --- To-do: count user's active projects */}
+                  99
+                </Badge>
+              }>
+              Proyectos
+            </Tabs.Tab>
+
+            {/* !user.requestEnrollmentCount && ( --- To-do: set user's requests */}
+            {true && (
+              <Tabs.Tab
+                value={UserDetailsTabs.Requests}
+                leftSection={<IconSend />}
+                rightSection={
+                  <Badge color="blue" variant="filled">
+                    {/* user.requestEnrollmentCount --- To-do: count user's requests */}
+                    99
+                  </Badge>
+                }>
+                Solicitudes
+              </Tabs.Tab>
+            )}
+          </Tabs.List>
+
+          {/*
+          <Tabs.Panel value={UserDetailsTabs.Projects}>
+            <EnrollmentList projectId={props.id} enrollments={user.enrollments} isAdmin={!user.requestEnrollmentCount} />
+          </Tabs.Panel>
+          */}
+
+          {!errorEnrollmentRequests && enrollmentRequests && (
+            <Tabs.Panel value={UserDetailsTabs.Requests}>
+              {isLoadingEnrollmentRequests ? (
+                <Tabs.Tab value={UserDetailsTabs.Requests}>
+                  <Loader type="dots" />
+                </Tabs.Tab>
+              ) : (
+                enrollmentRequests.enrollmentRequests.map((request: EnrollmentRequestShow) => (
+                  <Card key={request.id} p="md" mt="md" withBorder>
+                    <Text size="lg" w={500}>
+                      {request.user.firstName} {request.user.lastName}
+                    </Text>
+
+                    <Group mt="xs" gap="xs">
+                      {request.user.userAffiliations.map((affiliation: UserAffiliation) => (
+                        <Badge
+                          key={affiliation.id}
+                          color="pink.6"
+                          variant="light"
+                          component="button"
+                          style={{ cursor: 'pointer' }}
+                          onClick={(event) => {
+                            event.stopPropagation()
+                            handleDepartmentBadgeClick(
+                              affiliation.researchDepartment.facility.institution.id,
+                              affiliation.researchDepartment.facility.id,
+                              affiliation.researchDepartment.id
+                            )
+                          }}>
+                          {affiliation.researchDepartment.facility.institution.abbreviation} |{' '}
+                          {affiliation.researchDepartment.facility.abbreviation} |{' '}
+                          {affiliation.researchDepartment.name}
+                        </Badge>
+                      ))}
+                    </Group>
+
+                    <Group mt="xs" gap="xs">
+                      {request.user.interests.map((interest: Interest) => (
+                        <Badge
+                          variant="dot"
+                          key={interest.id}
+                          color="blue.6"
+                          size="lg"
+                          style={{ cursor: 'pointer' }}
+                          onClick={(event) => {
+                            event.stopPropagation()
+                            handleInterestTagClick(interest.id)
+                          }}>
+                          {interest.name}
+                        </Badge>
+                      ))}
+                    </Group>
+                  </Card>
+                ))
+              )}
+            </Tabs.Panel>
+          )}
+        </Tabs>
       </Card>
     </>
   )
--- components/User/UserItem.tsx
+++ components/User/UserItem.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { Badge, Card, Chip, Flex, Group, Text, useMantineColorScheme } from '@mantine/core'
+import { IconUser } from '@tabler/icons-react'
 import styles from './UserItem.module.css'
 
 import { CurrentUserQueryOptions } from '@/services/currentUser'
@@ -9,7 +10,7 @@ import { Url } from '@/services/url'
 
 import InfoMessage from '../Common/InfoMessage/InfoMessage'
 import User from '@/entities/User'
-import { IconUser } from '@tabler/icons-react'
+import { UserDetailsTabs } from './UserDetails'
 
 interface UserItemProps {
   user?: User
@@ -41,7 +42,7 @@ const UserItem = (props: UserItemProps) => {
       mb="0.5rem"
       p="md"
       radius="md"
-      onClick={() => router.push(`/users/${user.id}`)}
+      onClick={() => router.push(`/users/${user.id}?activeTab=${UserDetailsTabs.Projects}`)}
       className={`${styles.card} ${colorScheme == 'dark' ? styles.cardDark : styles.cardLight}`}>
       <div style={{ width: '100%' }}>
         <Flex direction={'row'} align={'center'} gap={10}>
