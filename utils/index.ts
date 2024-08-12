499d5d08391d4a206702fe3ebf229e727c09d739
Alejandro Recalde <alejandrorecalde5@gmail.com>
2024-08-11 23:24:54 -0300

    Refactor: Remove unnecessary code

--- utils/index.ts
+++
@@ -1,9 +0,0 @@
-//////////// EXPORT ALL TOOLS ////////////
-import Constants from './string/Constants'
-import Dates from './string/Dates'
-import Env from './config/Env'
-import RegEx from './string/RegEx'
-import Validation from './string/Validation'
-
-const Tools = { Constants, Dates, Env, RegEx, Validation }
-export default Tools
