aec48d20b4b099fbc4d16525a768e01c80c8dd4c
Alejandro Recalde <alejandrorecalde5@gmail.com>
2024-08-11 23:26:01 -0300

    Add Mantine React Table package and styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import '@mantine/core/styles.css'
 import '@mantine/dates/styles.css'
 import '@mantine/notifications/styles.css'
 import '@mantine/tiptap/styles.css'
+import 'mantine-react-table/styles.css'
 import '@/styles/globals.scss'
 
 import Theme from './theme'
