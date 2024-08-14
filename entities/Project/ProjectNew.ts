c21dc90f156f92c39460528593f321874a566d18
NicoAntonelli <niconelli2@gmail.com>
2024-08-14 02:07:21 -0300

    Projects: new project req & res entities

---
+++ entities/Project/ProjectNew.ts
@@ -0,0 +1,34 @@
+import Enrollment from '@/entities/Enrollment/Enrollment'
+import Interest from '@/entities/Interest'
+import Language from '@/entities/HelpTypes/Language'
+import ProjectType from './ProjectType'
+import ResearchDepartment from '@/entities/ResearchDepartment'
+
+export interface ProjectNewRequest {
+  name: string
+  type: ProjectType
+  language: Language
+  description?: string
+  endDate?: string
+  web?: string
+  requesterMessage?: string
+
+  interestsIds: number[]
+  interestsToCreate: string[]
+  researchDepartmentsIds: number[]
+}
+
+export interface ProjectNewResponse {
+  id: number
+  name: string
+  type: ProjectType
+  language: Language
+  description?: string
+  creationDate: string
+  endDate?: string
+  web?: string
+
+  researchDepartments: ResearchDepartment[]
+  interests: Interest[]
+  enrollments: Enrollment[]
+}
