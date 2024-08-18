d8ec8ff35c8dbaa862905a91bc3150e56bd1cf6b
NicoAntonelli <niconelli2@gmail.com>
2024-08-18 14:57:27 -0300

    About: add 'report bug' buttons inside error-handling components

--- components/Common/Error/ErrorGeneric.tsx
+++ components/Common/Error/ErrorGeneric.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import Link from 'next/link'
 
-import { Button, Flex, Text } from '@mantine/core'
+import { Button, Flex, Group, Text } from '@mantine/core'
 import Theme from 'src/app/theme'
 
 interface ErrorProps {
@@ -15,20 +15,27 @@ const ErrorGeneric = (props: ErrorProps) => {
   }, [props.error])
 
   return (
-    <Flex direction={'column'} align={'center'}>
-      <Text mt={50} size={'2.5rem'}>
+    <Flex mt={'2rem'} direction={'column'} align={'center'}>
+      <Text mt={'2rem'} size={'2.5rem'}>
         UNIVERSITEAMS
       </Text>
       <Text size={'6rem'}>Error</Text>
-      <Text my={'2rem'} pt={'2rem'} style={{ borderTop: `1px solid $'orange.4'` }}>
+      <Text mt={'2rem'} pt={'2rem'} style={{ borderTop: `1px solid $'orange.4'` }}>
         Ha ocurrido un problema en la pantalla anterior
       </Text>
-      <Button mb={20} color="orange.4" onClick={() => props.reset()}>
+      <Group mt={'2rem'}>
+        <Link href="/">
+          <Button color={Theme.primaryColor}>Volver al inicio</Button>
+        </Link>
+      </Group>
+      <Group mt={'2rem'}>
+        <Link href="/about#reportBug">
+          <Button color="orange.6">Reportar bug</Button>
+        </Link>
+      </Group>
+      <Button mt={'2rem'} color="orange.6" onClick={() => props.reset()}>
         Intentar nuevamente
       </Button>
-      <Link href="/">
-        <Button color={Theme.primaryColor}>Volver al inicio</Button>
-      </Link>
     </Flex>
   )
 }
--- components/Common/Error/ErrorNotFound.tsx
+++ components/Common/Error/ErrorNotFound.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import Link from 'next/link'
 
-import { Button, Flex, Text } from '@mantine/core'
+import { Button, Flex, Group, Text } from '@mantine/core'
 import Theme from 'src/app/theme'
 
 const ErrorNotFound = () => {
   return (
-    <Flex direction={'column'} align={'center'}>
-      <Text mt={50} size={'2.5rem'}>
+    <Flex mt={'2rem'} direction={'column'} align={'center'}>
+      <Text mt={'2rem'} size={'2.5rem'}>
         UNIVERSITEAMS
       </Text>
       <Text size={'8rem'}>404</Text>
-      <Text my={'2rem'} pt={'2rem'} style={{ borderTop: '1px solid orange' }}>
+      <Text mt={'2rem'} pt={'2rem'} style={{ borderTop: '1px solid orange' }}>
         Error: Página o recurso no encontrado
       </Text>
-      <Link href="/">
-        <Button color={Theme.primaryColor}>Volver al inicio</Button>
-      </Link>
+      <Group mt={'2rem'}>
+        <Link href="/">
+          <Button color={Theme.primaryColor}>Volver al inicio</Button>
+        </Link>
+      </Group>
+      <Group mt={'2rem'}>
+        <Link href="/about#reportBug">
+          <Button color="orange.6">Reportar bug</Button>
+        </Link>
+      </Group>
     </Flex>
   )
 }
