721294743b14ad6a9ec40e581453009c225c40e7
NicoAntonelli <niconelli2@gmail.com>
2024-08-29 16:58:29 -0300

    Dates: re-order functions

--- utils/string/Dates.ts
+++ utils/string/Dates.ts
@@ -1,14 +1,14 @@
 //////////// DATE TOOLS ////////////
 
+// Get current date & time with a filename-friendly format
+const getDateTimeShort = () => {
+  return new Date().toISOString().replace(/:/g, '-').replace('T', '_').split('.')[0]
+}
+
 // A simple date formatter function to format the date strings
 const formatDate = (dateString: string | undefined) => {
   if (!dateString) return 'N/A'
   return new Date(dateString).toLocaleDateString()
-}
-
-// Get current date & time with a filename-friendly format
-const getDateTimeShort = () => {
-  return new Date().toISOString().replace(/:/g, '-').replace('T', '_').split('.')[0]
 }
 
 const Dates = { getDateTimeShort, formatDate }
