d766a9e7d9d43578c73183ca1b1fc863775eee89
Alejandro Recalde <alejandrorecalde5@gmail.com>
2024-08-29 19:06:27 -0300

    Revert "Refactor help manual links and add conditional rendering based on user role"
    This reverts commit f272616211200d117564773af15b9938c33f4510.

--- components/Help/HelpManuals.tsx
+++ components/Help/HelpManuals.tsx
@@ -4,13 +4,8 @@ import { Anchor, Badge, Button, Card, Group, Image, Text } from '@mantine/core'
 import { useMediaQuery } from '@mantine/hooks'
 import Env from 'utils/config/Env'
 import Theme from 'src/app/theme'
-import { CurrentUserInfo, UserSystemRole } from '../../services/currentUser'
 
-interface HelpManualsProps {
-  currentUser?: CurrentUserInfo
-}
-
-const HelpManuals = ({ currentUser }: HelpManualsProps) => {
+const HelpManuals = () => {
   const isTablet = useMediaQuery(`(max-width: ${Theme.breakpoints?.lg})`)
   const isPhone = useMediaQuery(`(max-width: ${Theme.breakpoints?.sm})`)
 
@@ -67,15 +62,13 @@ const HelpManuals = ({ currentUser }: HelpManualsProps) => {
           Manual de usuario líder o administrador de proyecto
         </Link>
       </Button>
-      {currentUser?.systemRole === UserSystemRole.ADMIN && (
-        <Button color="orange.6" mt="md" radius="md">
-          <Link
-            href="https://px6qytsypezat9xj.public.blob.vercel-storage.com/Manual%20-%20Usuario%20Administrador%20de%20Sistema.pdf"
-            style={{ color: 'inherit', textDecoration: 'inherit' }}>
-            Manual de usuario administrador de sistema
-          </Link>
-        </Button>
-      )}
+      <Button color="orange.6" mt="md" radius="md">
+        <Link
+          href="https://px6qytsypezat9xj.public.blob.vercel-storage.com/Manual%20-%20Usuario%20Administrador%20de%20Sistema.pdf"
+          style={{ color: 'inherit', textDecoration: 'inherit' }}>
+          Manual de usuario administrador de sistema
+        </Link>
+      </Button>
     </Card>
   )
 }
