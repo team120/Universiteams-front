2fb18725c29e664704aabe2e7e0f522a85db596d
NicoAntonelli <niconelli2@gmail.com>
2024-09-15 19:21:42 -0300

    Users: simplify userInList DTO extending base user

--- entities/User/UserInList.ts
+++ entities/User/UserInList.ts
@@ -1,20 +1,9 @@
-import Enrollment from '@/entities/Enrollment/Enrollment'
-import Interest from '@/entities/Interest'
-import UserAffiliation from './UserAffiliation'
+import User from './User'
 
 export enum UserSortAttribute {
   LastName = 'lastName',
 }
 
-interface UserInList {
-  id: number
-  firstName: string
-  lastName: string
-  email: string
-  requestEnrollmentInvitationsCount: number
-  userAffiliations: UserAffiliation[]
-  interests: Interest[]
-  enrollments: Enrollment[]
-}
+interface UserInList extends User {}
 
 export default UserInList
